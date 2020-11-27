112d2614566f0b5ce7422bd13dfebba73c85b54d
James Robertson <jamesurobertson@gmail.com>
2020-11-26 21:12:48 -0600

    error handlers set up

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const { ValidationError } = require("sequelize");
 
 const { environment } = require("./config");
 const isProduction = environment === "production";
@@ -34,5 +35,36 @@ app.use(
 );
 
 app.use(routes);
+
+//resource not found error handler
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+//sequelize error handler - makes errors prettier
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
+
+// catch all error handler
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const apiRouter = require("./api");
 
 router.get("/hello/world", (req, res) => {
   res.cookie("CSRF-TOKEN", req.csrfToken());
   res.send("Hello World!");
 });
 
+router.use("/api", apiRouter);
 module.exports = router;
