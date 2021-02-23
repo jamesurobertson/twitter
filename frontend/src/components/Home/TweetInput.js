98262892502b057b3a3acf8582bb055ac354d689
James Robertson <jamesurobertson@gmail.com>
2021-02-23 15:05:47 -0600

    tagging users in input field working

--- frontend/src/components/Home/TweetInput.js
+++ frontend/src/components/Home/TweetInput.js
@@ -9,13 +9,15 @@ import Editor from "@draft-js-plugins/editor";
 import "@draft-js-plugins/mention/lib/plugin.css";
 
 const TweetInput = () => {
+  const [open, setOpen] = useState(true);
+  const [suggestions, setSuggestions] = useState([]);
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
-  const [open, setOpen] = useState(true);
-  const [suggestions, setSuggestions] = useState([]);
+
   const dispatch = useDispatch();
   const ref = useRef();
+
   const { MentionSuggestions, plugins } = useMemo(() => {
     const mentionPlugin = createMentionPlugin();
 
@@ -30,12 +32,10 @@ const TweetInput = () => {
 
   const onSearchChange = useCallback(({ value }) => {
     console.log(value);
-    // TODO: FIND FIX FOR THIS.
-    let empty = value.length === 0;
-    fetch(`/api/users/search/${empty ? "_" : value}`)
+    if (value.length === 0) return setSuggestions([]);
+    fetch(`/api/users/search/${value}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         const newData = data.map((user) => ({
           name: `@${user.username}`,
           link: `/profile/${user.id}`,
@@ -51,7 +51,6 @@ const TweetInput = () => {
   //     setContent("");
   //   };
 
-  // todo: find some library to change from input type to be more dynamic.
   return (
     <div
       className="p-2 border-solid border-2 border-black"
@@ -69,9 +68,6 @@ const TweetInput = () => {
         onOpenChange={onOpenChange}
         suggestions={suggestions}
         onSearchChange={onSearchChange}
-        onAddMention={() => {
-          //get mention object selected
-        }}
       />
     </div>
   );
