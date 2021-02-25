b3765dcffdd797427b3068411a0d06e25b5f09f0
James Robertson <jamesurobertson@gmail.com>
2021-02-24 20:08:02 -0600

    removed null from showing if no user details

--- frontend/src/components/Profile/ProfileNames.js
+++ frontend/src/components/Profile/ProfileNames.js
@@ -1,7 +1,7 @@
 const ProfileNames = ({ firstName, lastName, username }) => {
   return (
     <div className="mb-2">
-      <div className="font-bold ">{`${firstName} ${lastName}`}</div>
+      <div className="font-bold ">{`${firstName || ""} ${lastName || ""}`}</div>
       <div className="font-sm font-light">@{username}</div>
     </div>
   );
